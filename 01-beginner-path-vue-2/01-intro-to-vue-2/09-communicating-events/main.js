c52755d075ec2ac479cec1fe5e387fb5ef225d84
heyfaraaiden <heyfaraaiden@gmail.com>
2021-12-19 13:56:49 +0700

    Finish Intro to Vue 2 - 09. Communicating Events

---
+++ 01-beginner-path-vue-2/01-intro-to-vue-2/09-communicating-events/main.js
@@ -0,0 +1,122 @@
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+  <div class="product">
+    <div class="product-image">
+      <img v-bind:src="image">
+    </div>
+    
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if="inStock">In Stock</p>
+      <p v-else>Out of Stock</p>
+      <p>Shipping: {{ shipping }}</p>
+
+      <ul>
+        <li v-for="detail in details">{{ detail }}</li>
+      </ul>
+      
+      <div class="color-box"
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+      >
+      </div>
+      
+      <button v-on:click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }">
+        Add to Cart
+      </button>
+
+      <button @click="removeFromCart">
+        Remove from Cart
+      </button>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      details: ['80% cotton', '20% polyster', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
+        }
+      ],
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+    },
+    updateProduct(index) {
+      this.selectedVariant = index;
+    },
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      if(this.premium) {
+        return 'Free';
+      }
+      return 2.99;
+    }
+  }
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    removeItem(id) {
+      const indexOfItemToBeRemoved = this.cart.indexOf(id);
+      this.cart.splice(indexOfItemToBeRemoved, 1);
+      // for(var i = this.cart.length - 1; i >= 0; i--) {
+      //   if (this.cart[i] === id) {
+      //      this.cart.splice(i, 1);
+      //   }
+      // }
+    }
+  }
+});
+
+
+/* NOTE:
+SEE MORE ON:
+1. https://www.notion.so/VueJS-1ed318cc47ff47a7941a312316696567
+*/
